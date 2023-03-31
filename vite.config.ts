c052a192d446b3eff0f244911d65fedb43356f30
Juan Carlos Valdés <idevkingos@gmail.com>
2023-03-31 14:37:32 -0300

    feat: table builder with react table

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://jcvaldes.github.io/react-table-dynamic-demo/',
   plugins: [react()],
 })
